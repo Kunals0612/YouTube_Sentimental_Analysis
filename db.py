7f2c1ed3d3c9e0c2d01e3466cce1f64ec0afc986
Kunall <146503735+Kunals0612@users.noreply.github.com>
2024-06-15 12:23:52 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,4 +1,4 @@
 import mysql.connector
-conn = mysql.connector.connect(host='localhost', password='Kunals#2004', user='root', database='youtube')
+conn = mysql.connector.connect(host='localhost', password='', user='root', database='youtube')
 if(conn.is_connected()):
     print("connected")
